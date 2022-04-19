1ab195274ce98daa7c61bccfc6eefc447ebd5242
Rihards <76222397+RihardsKK@users.noreply.github.com>
2022-04-19 16:59:40 +0300

    Fibonachi numbers as much as you want

---
+++ fibofunctions.js
@@ -0,0 +1,12 @@
+
+
+var n1 = 0 , n2 = 1, next_num, i;
+var num = parseInt (prompt (" Ievadi Fibonachi virknes garumu")); 
+document.write( "fibonačī virkne -->");
+for ( i = 1; i <= num; i++)
+{  document.write(" <br> " + n1);
+next_num = n1 + n2
+n1 =n2
+n2 = next_num;
+
+}
